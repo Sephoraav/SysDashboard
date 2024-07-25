d6832e0a7f24910add7997eb92d2d359b79697a4
EC2 Default User <ec2-user@ip-172-31-45-233.eu-north-1.compute.internal>
2024-07-25 18:47:27 +0000

    july24 update

--- ShellScripts/DiskSpace.sh
+++ ShellScripts/DiskSpace.sh
@@ -1,3 +1,9 @@
-
 #!/bin/bash
-df -h | grep '^/dev/' | awk '{ print $5 " " $1 }'
+
+echo " "
+echo -e "\033[0;32mDISK DATA\033[0m"
+echo " "
+echo -e "\033[0;34mDISK SPACE UTILISATION\033[0m"
+
+df --total -h | grep "total" | awk '{print $1 " " $2 " " $3 " " $4 " "$5}'
+
